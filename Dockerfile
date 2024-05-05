150ed310350f3acf29fe5048ef3fab6b6dd4eb7a
janole <ole@mobileways.de>
2024-05-05 08:22:39 +0200

    Add first working prototype / PoC

--- Dockerfile
+++ Dockerfile
@@ -3,18 +3,36 @@ FROM debian:bookworm-slim
 LABEL maintainer="Jan Ole Suhr <ole@janole.com>"
 
 RUN true \
-#
-#
-#
+    #
+    #
+    #
     && apt-get update && apt-get -y upgrade \
-#
-#
-#
-    && apt-get install -y openvpn iptables dumb-init \
-#
-#
-#
+    #
+    #
+    #
+    && apt-get install -y openvpn iptables dumb-init gettext \
+    #
+    #
+    #
     && rm -rf /var/lib/apt/lists/*
 
+ENV CONFDIR=/conf
+ENV VPNDIR=${CONFDIR}/openvpn
+ENV CLIENTSDIR=${CONFDIR}/clients
+ENV TEMPLATESDIR=/.templates
+
+ENV VPNCAFILE=${VPNDIR}/ca.crt
+ENV VPNKEYFILE=${VPNDIR}/vpn.key
+ENV VPNCERTFILE=${VPNDIR}/vpn.crt
+ENV VPNDHFILE=${VPNDIR}/dh.pem
+ENV VPNTAKEYFILE=${VPNDIR}/ta.key
+
+ENV UDPCONF=${VPNDIR}/udp-server.conf
+ENV TCPCONF=${VPNDIR}/tcp-server.conf
+
+COPY ./rules.v4 /etc/iptables/rules.v4
+COPY ./conf/openvpn ${TEMPLATESDIR}/
+COPY ./init-certs.sh /init-certs.sh
+
 ENTRYPOINT ["/usr/bin/dumb-init", "--"]
-CMD ["bash", "-c", "iptables-restore < /etc/iptables/rules.v4 && exec openvpn --config /etc/openvpn/server.conf"]
+CMD ["bash", "-c", "iptables-restore < /etc/iptables/rules.v4 && exec openvpn --config /conf/openvpn/tcp-server.conf"]
---
+++ init-certs.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+CADIR="${CONFDIR}/ca"
+CAKEYFILE="${CADIR}/private/ca.key"
+CACERTFILE="${CADIR}/public/ca.crt"
+
+createDH()
+{
+	if [ -f ${VPNDHFILE} ];
+	then
+		return
+	fi
+
+	mkdir -p `dirname ${VPNDHFILE}`
+	openssl dhparam -out ${VPNDHFILE} 2048
+}
+
+createTA()
+{
+	if [ -f ${VPNTAKEYFILE} ];
+	then
+		return
+	fi
+
+	mkdir -p `dirname ${VPNTAKEYFILE}`
+	openvpn --genkey secret ${VPNTAKEYFILE}
+	sed -i '/#.*$/d' ${VPNTAKEYFILE}
+}
+
+createCA()
+{
+	if [ -f ${CAKEYFILE} ] && [ -f ${CACERTFILE} ];
+	then
+		return
+	fi
+
+	mkdir -p `dirname ${CAKEYFILE}`
+	openssl genrsa -out ${CAKEYFILE} 4096
+	mkdir -p `dirname ${CACERTFILE}`
+
+	CACONF="${TEMPLATESDIR}/ca.conf.template"
+	envsubst < ${CACONF} | openssl req -config - -x509 -new -nodes -extensions v3_ca -key ${CAKEYFILE} -sha256 -days 3650 -out ${CACERTFILE}
+}
+
+createVPN()
+{
+	if [ -f ${VPNKEYFILE} ] && [ -f ${VPNCERTFILE} ];
+	then
+		return
+	fi
+
+	mkdir -p `dirname ${VPNCAFILE}`
+	cp ${CACERTFILE} ${VPNCAFILE}
+
+	mkdir -p `dirname ${VPNKEYFILE}`
+	openssl genrsa -out ${VPNKEYFILE} 4096
+
+	VPNCONF="${TEMPLATESDIR}/vpn.conf.template"
+	VPNCSRFILE="${VPNDIR}/vpn.csr"
+	mkdir -p `dirname ${VPNCSRFILE}`
+	envsubst < ${VPNCONF} | openssl req -config - -new -key ${VPNKEYFILE} -out ${VPNCSRFILE}
+	openssl x509 -req -in ${VPNCSRFILE} -CA ${CACERTFILE} -CAkey ${CAKEYFILE} -CAcreateserial -out ${VPNCERTFILE} -days 500 -sha256
+
+	createTA
+	createDH
+
+	# openssl verify -verbose -CAfile ${CACERTFILE} ${VPNCERTFILE}
+}
+
+createServerConfig()
+{
+	SERVERCONF="${TEMPLATESDIR}/server.conf.template"
+	VPNPROTO=tcp envsubst < ${SERVERCONF} > ${TCPCONF}
+	VPNPROTO=udp envsubst < ${SERVERCONF} > ${UDPCONF}
+}
+
+createCLIENT()
+{
+	CLIENTCONF=${TEMPLATESDIR}/client.conf.template
+	TCPCLIENTCONF=${TEMPLATESDIR}/tcp.ovpn.template
+
+	NAME=${CLIENT_CN}
+
+	local DIR="${CLIENTSDIR}/${NAME}"
+	local KEYFILE="${DIR}/${NAME}.key"
+	local CSRFILE="${DIR}/${NAME}.csr"
+	local CERTFILE="${DIR}/${NAME}.crt"
+
+#	if [ -f ${KEYFILE} ] && [ -f ${CERTFILE} ];
+#	then
+#		return
+#	fi
+
+	mkdir -p ${DIR}
+
+	openssl genrsa -out ${KEYFILE} 4096
+	envsubst < ${CLIENTCONF} | openssl req -config - -new -key ${KEYFILE} -out ${CSRFILE}
+	openssl x509 -req -in ${CSRFILE} -CA ${CACERTFILE} -CAkey ${CAKEYFILE} -CAcreateserial -out ${CERTFILE} -days 365 -sha256
+
+	export CACERT=`cat $CACERTFILE`
+	export CLIENTKEY=`cat $KEYFILE`
+	export CLIENTCERT=`cat $CERTFILE`
+	export VPNTAKEY=`cat $VPNTAKEYFILE`
+	envsubst < $TCPCLIENTCONF > ${DIR}/${NAME}.ovpn
+}
+
+createCA
+
+createVPN
+
+createCLIENT
+
+createServerConfig
