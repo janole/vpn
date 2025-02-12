03efdb0a958a23131c7e0f70243f9bd23a6a215b
janole <ole@mobileways.de>
2025-02-12 11:16:21 +0100

    Fix version script to remove/replace build number

--- version.sh
+++ version.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-echo "$(git describe --abbrev=0 --tags).$(git rev-list HEAD --count)"
+echo "$(git describe --abbrev=0 --tags | awk -F. '{ print $1 "." $2 }').$(git rev-list HEAD --count)"
